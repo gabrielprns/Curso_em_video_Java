c0b95825838fa6dc5117331fd3e4950c2f6afc71
gabrielprns <gabrielpedrornn@outlook.com>
2022-01-08 20:51:10 -0300

    Project

---
+++ src/aula13ex1/para.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aula13ex1;
+
+/**
+ *
+ * @author Usuario
+ */
+public class para {
+    
+    public static void main(String[] args) {
+        int i;
+        for(i =0; i<=100; i+=10){
+            System.out.println(i);
+        }
+        
+    }
+    
+}
