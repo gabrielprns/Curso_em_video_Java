32df4f6c735c80b8e3a14d58df2b0ba6b75f0964
gabrielprns <gabrielpedrornn@outlook.com>
2022-01-15 23:56:16 -0300

    Project

---
+++ src/Aula15ex1/operacao2.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Aula15ex1;
+
+/**
+ *
+ * @author Usuario
+ */
+public class operacao2 {
+    
+}
