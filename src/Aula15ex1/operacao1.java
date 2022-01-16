d781fe08e4f3818b6721a1dc94a2fee73600ebf1
gabrielprns <gabrielpedrornn@outlook.com>
2022-01-15 23:48:59 -0300

    Project

---
+++ src/Aula15ex1/operacao1.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Aula15ex1;
+
+/**
+ *
+ * @author Usuario
+ */
+public class operacao1 {
+    String soma(int i, int f){
+        String s="";
+        for(int c=1;c<=f;c++){
+            s+=c;
+        }
+      return s;  
+
+}
+    
+}
