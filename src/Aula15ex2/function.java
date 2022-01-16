3f08b7830c12951b121301929ce1483488c76b6c
gabrielprns <gabrielpedrornn@outlook.com>
2022-01-16 20:26:09 -0300

    Project

---
+++ src/Aula15ex2/function.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Aula15ex2;
+
+/**
+ *
+ * @author Usuario
+ */
+public class function {
+    public static void main(String[] args) {
+        fatorial f = new fatorial();
+        f.setValor(7);
+        System.out.println(f.getFormula() + f.getFatorial());
+        
+    }
+    
+}
---
+++ src/Aula15ex3/fatorial.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Aula15ex3;
+
+/**
+ *
+ * @author Usuario
+ */
+public class fatorial {
+    private int num=0;
+    private int fat=1;
+    private String formula="";
+    
+    public void setValor(int n){
+        num=n ;
+        int fa=1;
+        String s= "";
+        for(int c=n; c>1; c--){
+            fa*=c;
+            s+= c + " X ";
+                
+        }
+        s += "1= ";
+        fat=fa;
+        formula=s;
+    
+    }
+    
+     public int getFatorial(){
+        return fat;
+     
+     }
+     
+     public String getFormula(){
+         return formula ;
+     
+     }
+}
